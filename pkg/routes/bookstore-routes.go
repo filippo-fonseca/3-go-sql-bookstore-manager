bb30b108a7b4e3c793ebf2538bdf5e6a0222d128
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-08 13:10:12 -0600

    DB progress

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/filippo-fonseca/3-go-sql-bookstore-manager/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
+}
