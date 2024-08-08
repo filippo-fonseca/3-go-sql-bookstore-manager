a880fc822c6a1fdc79c80fb0800ab7cac61df245
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-08 13:23:02 -0600

    initial main.go file

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,14 +1,14 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm",
 	"github.com/filippo-fonseca/3-go-sql-bookstore-manager/pkg/config"
+	"github.com/jinzhu/gorm"
 )
 
 var db *gorm.DB
 
 type Book struct {
-	gorm.model,
+	gorm.Model
 	Name string `gorm:""json:"name"`
 	Author string `json:"author"`
 	Publication string `json:"publication"`
