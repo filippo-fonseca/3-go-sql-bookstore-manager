e6a30df1a08f50aea11db94a3a1cb534e050c4f2
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-08 13:27:14 -0600

    initial crud funcs

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -19,3 +19,30 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+
+	return book
+}
+
